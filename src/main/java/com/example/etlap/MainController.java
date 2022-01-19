af01b1be766e8e1112d44ca4e1d14c39a4ee4529
Schmidt Gergő <s.gergo.az@gmail.com>
2022-01-19 16:09:29 +0100

    etlap feltolt

--- src/main/java/com/example/etlap/MainController.java
+++ src/main/java/com/example/etlap/MainController.java
@@ -1,8 +1,15 @@
 package com.example.etlap;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainController {
     @FXML
@@ -21,7 +28,19 @@ public class MainController {
     public Spinner ftEmelesInput;
     @FXML
     public Spinner szazalekEmelesInput;
+    private EtlapDB db;
 
+    public void initialize(){
+        colNev.setCellValueFactory(new PropertyValueFactory<>("nev"));
+        colKategoria.setCellValueFactory(new PropertyValueFactory<>("kategoria"));
+        colAr.setCellValueFactory(new PropertyValueFactory<>("ar"));
+        try {
+            db = new EtlapDB();
+            etelListaFeltolt();
+        } catch (SQLException e) {
+            hibaKiir(e);
+        }
+    }
     public void onEtelFelveteleButtonClick(ActionEvent actionEvent) {
     }
 
@@ -38,5 +57,29 @@ public class MainController {
     }
 
     public void onKategoriaTorlesButtonClick(ActionEvent actionEvent) {
+    }
+    private void etelListaFeltolt(){
+        try {
+            List<Etel> etelList = db.getEtelek();
+            etlapTable.getItems().clear();
+            for(Etel etel: etelList){
+                etlapTable.getItems().add(etel);
+            }
+        } catch (SQLException e) {
+            hibaKiir(e);
+        }
+    }
+    protected void hibaKiir(Exception e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Hiba");
+        alert.setHeaderText(e.getClass().toString());
+        alert.setContentText(e.getMessage());
+        Timer alertTimer = new Timer();
+        alertTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(() -> alert.show());
+            }
+        }, 500);
     }
 }
